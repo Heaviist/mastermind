a4e3abf2835cc5aa851a4996ed9f5af547b575c1
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-16 17:36:52 +0200

    Add basic rules of the game for reference

---
+++ bulls_and_cows.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Below will be the rules of the game, for reference, taken as a starting point for creating the code.
+# Once the game is complete, the explanation will be given before play and deleted from this file.
+# 6 colours, 4 holes, 12 attempts
+# No blanks in both code and guesses, but duplicates are allowed.
+# Keep tally for: Colours in the correct position AND colours correct but not in the right position.
+# These values will be returned to the player after each turn
+# Win if you can guess within 12 attempts
